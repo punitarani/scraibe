3880613832f68c5a37606dc6411291b8bde13f64
Punit Arani <punitsai36@gmail.com>
2023-09-16 14:18:19 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,13 @@
+# Makefile
+
+# OS specific commands and variables
+ifeq ($(OS),Windows_NT)
+    SET_ENV = set
+else
+    SET_ENV = export
+endif
+
+
+format:
+	black *.py
+	black scraibe/
---
+++ main.py
@@ -0,0 +1,15 @@
+"""main.py"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
