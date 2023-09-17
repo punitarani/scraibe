faee0f6cd4d23fbb3ce557a6319d955d5d046971
Punit Arani <punitsai36@gmail.com>
2023-09-16 23:24:14 -0400

    Add SUBJECT_ID

--- scraibe/__init__.py
+++ scraibe/__init__.py
@@ -9,6 +9,8 @@ from config import PROJECT_DIR
 
 load_dotenv(PROJECT_DIR.joinpath(".env"))
 
+SUBJECT_ID = 10014354
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable not set"
 openai.api_key = OPENAI_API_KEY
